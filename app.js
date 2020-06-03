62ada457296755a01e17179e9507f44e7411a8bd
furki911 <55999845+furki911@users.noreply.github.com>
2020-06-03 21:47:53 +0530

    score-limit-100

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
       document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
       
       // Check if player WON the Game
-      if (scores[activePlayer] >= 20) {
+      if (scores[activePlayer] >= 100) {
          document.getElementById('name-' + activePlayer).textContent = 'WINNER!';
          document.querySelector('.dice').style.display = 'none';
          document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
